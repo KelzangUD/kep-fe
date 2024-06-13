be1d4302e93748a7fbcf1e46b6a934dba1f7abb6
KelzangUD <kuding755@gmail.com>
2024-06-13 14:58:40 +0600

    yearly data display C

--- src/component/admin/admin_dashboard/YearGraph.jsx
+++ src/component/admin/admin_dashboard/YearGraph.jsx
@@ -1,5 +1,15 @@
-import React from "react";
-import { Box, Container, Typography, Grid, Card } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Container,
+  Typography,
+  Grid,
+  Card,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@mui/material";
 import {
   BarChart,
   Bar,
@@ -9,105 +19,138 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import Route from "../../../routes/Route";
 
-const data = [
-  {
-    name: "July Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Aug Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Sept Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Oct Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Nov Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Dec Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "July Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Aug Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Sept Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Oct Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Nov Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-  {
-    name: "Dec Test",
-    excel: 40,
-    good: 14,
-    average: 24,
-    failed: 10,
-  },
-];
+// const data = [
+//   {
+//     name: "July Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Aug Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Sept Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Oct Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Nov Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Dec Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "July Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Aug Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Sept Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Oct Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Nov Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+//   {
+//     name: "Dec Test",
+//     excel: 40,
+//     good: 14,
+//     average: 24,
+//     failed: 10,
+//   },
+// ];
 
 const YearGraph = () => {
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [yearlyData, setYearlyData] = useState([]);
+  const yearHandle = (e) => {
+    setYear(e.target.value);
+  };
+  const token = localStorage.getItem("token");
+  const fetchYearlyData = async () => {
+    const res = await Route("GET", `/results/yearly_result/${year}`, token, null, null);
+    if (res?.status === 200) {
+      setYearlyData(res?.data?.results);
+    };
+  };
+  useEffect(() => {
+    fetchYearlyData();
+  }, [year]);
   return (
     <Box>
       <Typography variant="subtitle1">One Year Report</Typography>
       <Grid item xs={12}>
+        <Grid sx={{ display: "flex", justifyContent: "flex-end", py: "4px" }}>
+          <Box>
+            <FormControl>
+              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                defaultValue={year}
+                label="Year"
+                onChange={yearHandle}
+              >
+                <MenuItem value={2023}>2023</MenuItem>
+                <MenuItem value={2024}>2024</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Grid>
         <Box sx={{ display: "flex", alignItems: "left" }}>
           <Box>
             <BarChart
               width={1140}
               height={300}
-              data={data}
+              data={yearlyData}
               margin={{
                 top: 5,
                 right: 30,
