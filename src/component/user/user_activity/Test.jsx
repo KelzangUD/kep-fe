b5d90fac3eac579871df574228c3246e00f9f1a3
KelzangUD <kuding755@gmail.com>
2025-08-02 10:05:52 +0600

    test u

--- src/component/user/user_activity/Test.jsx
+++ src/component/user/user_activity/Test.jsx
@@ -52,12 +52,14 @@ const Test = () => {
         "You can't take the test at this time because test is not opened."
       );
       setShowNotification(true);
-    }else if (param?.row?.end_date <= new Date().toJSON()) {
-      setMessage(
-        "You're unable to take the test as it is closed."
-      );
-      setShowNotification(true);
-    } else {
+    }
+    // else if (param?.row?.end_date < new Date().toJSON()) {
+    //   setMessage(
+    //     "You're unable to take the test as it is closed."
+    //   );
+    //   setShowNotification(true);
+    // } 
+    else {
       fetchQuestions(param?.row?.id, param?.row);
     }
   };
